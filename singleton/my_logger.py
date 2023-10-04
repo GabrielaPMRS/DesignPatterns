e789685ff3318507699c53b808c6decb70cef536
GabrielaPMRS <gabrielasoares.ec@gmail.com>
2023-10-03 22:40:16 -0300

    Adicionar o arquivo de logger
    Exemplo do uso de Singleton

---
+++ singleton/my_logger.py
@@ -0,0 +1,27 @@
+import logging
+
+class MyLogger:
+    _logger = None
+
+    def __new__(cls):
+        if cls._logger is None:
+
+            print("Logger new")
+            cls._logger = logging.getLogger("logger_name")
+            cls._logger.setLevel(logging.DEBUG)
+            formatter = logging.Formatter(
+                '%(asctime)s \t [%(levelname)s | %(filename)s:%(lineno)s] > %(message)s')
+
+            streamHandler = logging.StreamHandler()
+
+            streamHandler.setFormatter(formatter)
+
+            cls._logger.addHandler(streamHandler)
+
+        return cls._logger
+
+if __name__ == "__main__":
+    logger1 = MyLogger()
+    logger1.info("Hello, Logger")
+    logger2 = MyLogger()
+    logger2.warning("bug occured")
